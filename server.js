7e1dce7514d9e04936e7b439f94abe41b75ef7b2
Ivan Gjorcev <16694600+callibra@users.noreply.github.com>
2024-05-12 19:13:10 +0200

    Update 555server.js
    555

--- server.js
+++ server.js
@@ -2,21 +2,6 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const kursSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    validate: validator.isEmail,
-    unique: [true, "Email mora biti jedinstven"],
-  },
-  type: {
-    type: String,
-    required: [true, "Mora da ima oblast"],
-    minlength: 1,
-    maxlength: [30, "Oblast je predugacka"],  ivan-1     bravooooooooooooooooooooooo   po 2 pat bravoooooooooooooooooooooo   po 3 pat bravooooooooooooooooooooooooo
-  },
-});
-owieryhnyt4ipoe5ypt4pypyvbuwetipoyvbuwetnipybunpwoetywbnetipybuwntpoibuywtp
-wetyweiouyvbwehnpiovbywenporiyvbwenpoivbywenpoivbywebnvipoywevnipowerybnwepoiyvb
-wetpyioweyvpowei5yvpweioyvbwernpoivywernipvbyeiowerywbnipoerybnweripoyvweripoyep
-const Akademija = mongoose.model("Kurs", kursSchema);
+
 
 module.exports = Akademija;
